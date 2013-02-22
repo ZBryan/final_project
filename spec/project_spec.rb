cf4642e4e35c8c854871b1929ab197b6fd336353
Zachary Bryan <zachx1@gmail.com>
2013-02-21 19:26:08 -0800

    inital commit

---
+++ spec/project_spec.rb
@@ -0,0 +1,21 @@
+project_spec.rb
+
+it 'should take an excel file and make an array'
+
+it 'should delete non esential data'
+
+it 'should return an array with esential information'
+
+it 'should put the class time once'
+
+it 'populates course levels with students first and last name'
+
+it 'populates name of level ie (pike, or p1)'
+
+it 'builds a second class of same name if larger than size limit'
+
+it 'if divided into two or more classes divides them equally'
+
+it 'exports to excel'
+
+it 'has class date range functionality'
